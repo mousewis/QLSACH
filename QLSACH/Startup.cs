806655c690d173df1eb096e50867e176412daed9
reino <mousewis@gmail.com>
2016-10-24 13:31:08 +0700

    Add project files.

---
+++ QLSACH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLSACH.Startup))]
+namespace QLSACH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
